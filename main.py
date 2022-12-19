f98ed90b3e061cf28af0d7899d39e3e8c5877a3c
LudwigCano1 <115447154+LudwigCano1@users.noreply.github.com>
2022-12-19 01:54:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ with st.sidebar:
     st.header("TRABAJO ESCALONADO 2")
     st.write("**Departamento Académico de Hidráulica e Hidrología**")
     st.write("**HH413G - IRRIGACIÓN**")
-    image= Image.open("img//UNI.PNG")
+    image= Image.open("img\UNI.PNG")
     st.image(image,width=200)
     st.write("**Integrantes:**")
     "- Caballero Lazarte Leidy Letzy"
