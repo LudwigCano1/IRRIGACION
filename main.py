c2ae538cfd6e60821e4e40aacc3d27c13ab9496d
LudwigCano1 <115447154+LudwigCano1@users.noreply.github.com>
2022-12-19 02:24:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 from math import ceil,sin,cos,tan,atan
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide",page_icon="💧")
 
 def discharge(x):
     tabla = [
